5b5de6d89bf113e16752e711506bd8d439ee4b2d
martycook <martinc@valornet.com>
2021-04-14 17:04:18 -0500

    I added a TryAddSingleton method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
 # CG.DependencyInjection change log
 ---
 
-## 2021.1 
+## 2021.2
+
+* I switched to the .NET 5.0 framework
+
+## 2021.1
 
 * I created the package.
 
--- src/CG.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/CG.DependencyInjection/ServiceCollectionExtensions.cs
@@ -265,6 +265,53 @@ namespace Microsoft.Extensions.DependencyInjection
             return serviceCollection;
         }
 
+        // *******************************************************************
+
+        /// <summary>
+        /// This method adds a singleton service of the type specified in 
+        /// <typeparamref name="TService"/> with an instance specified in the
+        /// <paramref name="instance"/> parameter, to the specified 
+        /// <see cref="IServiceCollection"/> object.
+        /// </summary>
+        /// <typeparam name="TService">The type of service to register.</typeparam>
+        /// <typeparam name="TImplementation">The type of implementation to use.</typeparam>
+        /// <param name="serviceCollection">The service collection to use for 
+        /// the operation.</param>
+        /// <param name="instance">The implementation instance to use for the 
+        /// operation.</param>
+        /// <returns>True if the type was registered successfully; false otherwise.</returns>
+        public static bool TryAddSingleton<TService, TImplementation>(
+            this IServiceCollection serviceCollection,
+            TImplementation instance
+            ) where TService : class
+              where TImplementation : class, TService
+        {
+            // Validate the parameters before attempting to use them.
+            Guard.Instance().ThrowIfNull(serviceCollection, nameof(serviceCollection))
+                .ThrowIfNull(instance, nameof(instance));
+
+            // Create a service descriptor for the interface type.
+            var serviceDescriptor = new ServiceDescriptor(
+                typeof(TService),
+                typeof(TImplementation),
+                ServiceLifetime.Singleton
+                );
+
+            // Is the type already registered?
+            if (true == serviceCollection.Contains(serviceDescriptor))
+            {
+                return false; // Nothing to do.
+            }
+
+            // Register the instance.
+            serviceCollection.AddSingleton<TService, TImplementation>(
+                (sp) => instance
+                );
+
+            // Return the results.
+            return true;
+        }
+
         #endregion
     }
 }
